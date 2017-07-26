56ee96bcfb981b2159ab57fb87fd45d1c100af31
Elad <eladad@gmail.com>
2017-07-26 17:33:21 +0300

    Checks pkts per second (instead of table refresh rate)

--- multi_process_custom_packet_basic/DPDK_Sender/sender.c
+++ multi_process_custom_packet_basic/DPDK_Sender/sender.c
@@ -26,12 +26,13 @@ static const struct rte_eth_conf port_conf_default = {
 
 #define MAX_PKT_COUNT 0xffff
 #define GET_RANDOM_BETWEEN(start, end) ((rand() % (end - start)) + start)
-#define REFRESH_TIME (2000) //in ms
-#define NUM_OF_TYPES (160)
+#define REFRESH_TIME (500) //in ms
+#define PKT_COUNTER_RESET_TIME (1000) //in ms
+#define NUM_OF_TYPES (16)
 
 /* basicfwd.c: Basic DPDK skeleton forwarding example. */
 static void print(int received, int total_recv, int sent, int total_sent,
-					unsigned int time_passed, unsigned int pkts_counted, unsigned int pkts_type_counter[])
+					unsigned int pkts_counted, unsigned int pkts_type_counter[])
 {
 	const char clr[] = { 27, '[', '2', 'J', '\0' };
 	const char topLeft[] = { 27, '[', '1', ';', '1', 'H','\0' };
@@ -48,8 +49,7 @@ static void print(int received, int total_recv, int sent, int total_sent,
 	printf("\nTotal packets sent overall: %d", total_sent);
 	printf("\n====================================================");
 	printf("\nPkts counted since last check : %d", pkts_counted);
-	printf("\ntime passed since last check : %d", time_passed);
-	printf("\nPkts/ms since last check : %d\n", (pkts_counted * 1000) / time_passed);
+	printf("\nPkts/ms since last check : %d\n", (pkts_counted * 1000) / PKT_COUNTER_RESET_TIME);
 
 	for (type_index = 0; type_index < NUM_OF_TYPES; type_index++)
 	{
@@ -225,9 +225,9 @@ static __attribute__((noreturn)) void lcore_main(struct rte_mempool * mbuf_pool)
 	int i = 0;
 	unsigned int  pkt_index = 0;
 	struct payload ready_payload;
-	clock_t curr_time, last_time;
+	clock_t curr_time, last_table_show_time, last_pkt_counter_reset;
 	unsigned int time_passed;
-	unsigned int pkts_counter;
+	unsigned int pkts_counter, last_pkts_counter;
 	unsigned int pkts_type_counter[NUM_OF_TYPES];
 
 
@@ -248,8 +248,10 @@ static __attribute__((noreturn)) void lcore_main(struct rte_mempool * mbuf_pool)
 
 
 	srand(time(NULL));
-	last_time = 0;
+	last_table_show_time = 0;
+	last_pkt_counter_reset = 0;
 	pkts_counter = 0;
+	last_pkts_counter = 0;
 	for (i = 0; i < NUM_OF_TYPES; i++)
 	{
 		pkts_type_counter[i] = 0;
@@ -259,7 +261,6 @@ static __attribute__((noreturn)) void lcore_main(struct rte_mempool * mbuf_pool)
 	for (;;) {
 		/* Get burst of RX packets, from first port of pair. */
 		curr_time = clock();
-		time_passed = ((curr_time - last_time) * 1000) / CLOCKS_PER_SEC;
 		
 		uint16_t nb_rx = 0;
 		struct rte_mbuf *bufs[BURST_SIZE];
@@ -286,11 +287,19 @@ static __attribute__((noreturn)) void lcore_main(struct rte_mempool * mbuf_pool)
 		total_sent += nb_tx;
 		pkts_counter += nb_tx;
 
-		if ((time_passed > REFRESH_TIME) && (nb_tx > 0))
+		time_passed = ((curr_time - last_pkt_counter_reset) * 1000) / CLOCKS_PER_SEC;
+		if (time_passed > PKT_COUNTER_RESET_TIME)
 		{
-			print(nb_rx, total_recv, nb_tx, total_sent, time_passed, pkts_counter, pkts_type_counter);
-			last_time = curr_time;
+			last_pkt_counter_reset = curr_time;
+			last_pkts_counter = pkts_counter;
 			pkts_counter = 0;
+		}
+
+		time_passed = ((curr_time - last_table_show_time) * 1000) / CLOCKS_PER_SEC;
+		if (time_passed > REFRESH_TIME)
+		{
+			print(nb_rx, total_recv, nb_tx, total_sent, last_pkts_counter, pkts_type_counter);
+			last_table_show_time = curr_time;
 		}
 
 		/* Free any unsent packets. */
