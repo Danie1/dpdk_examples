b3fe4c10261724a4c301b301cf316a7404271095
Elad <eladad@gmail.com>
2017-07-26 16:38:11 +0300

    Added statistics

--- multi_process_custom_packet_basic/DPDK_Sender/sender.c
+++ multi_process_custom_packet_basic/DPDK_Sender/sender.c
@@ -24,11 +24,18 @@ static const struct rte_eth_conf port_conf_default = {
 	.rxmode = { .max_rx_pkt_len = ETHER_MAX_LEN }
 };
 
+#define MAX_PKT_COUNT 0xffff
+#define GET_RANDOM_BETWEEN(start, end) ((rand() % (end - start)) + start)
+#define REFRESH_TIME (2000) //in ms
+#define NUM_OF_TYPES (160)
+
 /* basicfwd.c: Basic DPDK skeleton forwarding example. */
-static void print(int received, int total_recv, int sent, int total_sent)
+static void print(int received, int total_recv, int sent, int total_sent,
+					unsigned int time_passed, unsigned int pkts_counted, unsigned int pkts_type_counter[])
 {
 	const char clr[] = { 27, '[', '2', 'J', '\0' };
 	const char topLeft[] = { 27, '[', '1', ';', '1', 'H','\0' };
+	unsigned int type_index;
 
 	/* Clear screen and move to top left */
 	printf("%s%s", clr, topLeft);
@@ -39,6 +46,16 @@ static void print(int received, int total_recv, int sent, int total_sent)
 	printf("\nAggregate statistics ===============================");
 	printf("\nTotal packets received overall: %d", total_recv);
 	printf("\nTotal packets sent overall: %d", total_sent);
+	printf("\n====================================================");
+	printf("\nPkts counted since last check : %d", pkts_counted);
+	printf("\ntime passed since last check : %d", time_passed);
+	printf("\nPkts/ms since last check : %d\n", (pkts_counted * 1000) / time_passed);
+
+	for (type_index = 0; type_index < NUM_OF_TYPES; type_index++)
+	{
+		printf("type num %u:\t%u\n", type_index, pkts_type_counter[type_index]);
+	}
+
 	printf("\n====================================================\n");
 }
 
@@ -120,9 +137,7 @@ struct eth_header
 };
 
 
-#define MAX_PKT_COUNT 0xffff
-#define GET_RANDOM_BETWEEN(start, end) ((rand(time(NULL)) % (end - start + 1)) + start)
-#define REFRESH_TIME (2000) //in ms
+
 
 /**
 * Creates and returns a default payload
@@ -212,6 +227,8 @@ static __attribute__((noreturn)) void lcore_main(struct rte_mempool * mbuf_pool)
 	struct payload ready_payload;
 	clock_t curr_time, last_time;
 	unsigned int time_passed;
+	unsigned int pkts_counter;
+	unsigned int pkts_type_counter[NUM_OF_TYPES];
 
 
 	/*
@@ -232,6 +249,11 @@ static __attribute__((noreturn)) void lcore_main(struct rte_mempool * mbuf_pool)
 
 	srand(time(NULL));
 	last_time = 0;
+	pkts_counter = 0;
+	for (i = 0; i < NUM_OF_TYPES; i++)
+	{
+		pkts_type_counter[i] = 0;
+	}
 
 	/* Run until the application is quit or killed. */
 	for (;;) {
@@ -245,7 +267,8 @@ static __attribute__((noreturn)) void lcore_main(struct rte_mempool * mbuf_pool)
 
 		for (i = 0; i < BURST_SIZE; i++)
 		{
-			ready_payload = create_payload(rand()%16, pkt_index + i, "Read Me\n\0", 9);
+			ready_payload = create_payload(GET_RANDOM_BETWEEN(0, NUM_OF_TYPES), pkt_index + i, "Read Me\n\0", 9);
+			pkts_type_counter[ready_payload.type] += 1;
 			create_packet(&bufs[i], mbuf_pool, ready_payload);
 		}
 		nb_rx += BURST_SIZE;
@@ -261,11 +284,13 @@ static __attribute__((noreturn)) void lcore_main(struct rte_mempool * mbuf_pool)
 
 		nb_tx += rte_eth_tx_burst(PORT_SEND, 0,	bufs, BURST_SIZE);
 		total_sent += nb_tx;
+		pkts_counter += nb_tx;
 
 		if ((time_passed > REFRESH_TIME) && (nb_tx > 0))
 		{
-			print(nb_rx, total_recv, nb_tx, total_sent);
+			print(nb_rx, total_recv, nb_tx, total_sent, time_passed, pkts_counter, pkts_type_counter);
 			last_time = curr_time;
+			pkts_counter = 0;
 		}
 
 		/* Free any unsent packets. */
