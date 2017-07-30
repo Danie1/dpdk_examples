b97388d3b15c450df1393f387bc89ac28c141231
Daniel Saad <me@danie1.me>
2017-07-30 13:31:46 +0300

    Adding definitions.h which I forgot to add in previous commit

---
+++ multi_process_custom_packet_basic/shared/definitions.h
@@ -0,0 +1,39 @@
+/***********************************************
+* General Definitions
+************************************************/
+#define PORT_SEND (0)
+#define PORT_RECV (1)
+
+// Number of message types that exist
+#define NUM_OF_MSG_TYPES (16)
+/***********************************************
+* Message Payload Definitions
+************************************************/
+struct payload
+{
+    int type;
+    int index;
+    char msg[100];
+} payload;
+
+struct eth_header
+{
+    unsigned short int dst[3];
+    unsigned short int src[3];
+    unsigned short int protocol;
+} eth_header;
+
+/***********************************************
+* Port Statistics Definitions
+************************************************/
+struct port_info
+{
+    uint32_t value;
+} port_info;
+
+struct ports_statistics
+{
+	struct port_info total[NUM_OF_MSG_TYPES];
+	struct port_info dropped[NUM_OF_MSG_TYPES];
+	struct port_info current[NUM_OF_MSG_TYPES];
+} ports_statistics;
